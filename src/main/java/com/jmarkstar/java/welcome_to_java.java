8ed44559df36e1a5444becfbde1fc7e2b44508c3
jmarkstar <mestrella.1991@gmail.com>
2017-01-15 20:28:52 -0500

    starting

---
+++ src/main/java/com/jmarkstar/java/welcome_to_java.java
@@ -0,0 +1,12 @@
+package com.jmarkstar.java;
+
+/**
+ * Created by jmarkstar on 15/01/2017.
+ */
+public class welcome_to_java {
+
+    public static void main(String args []){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
